5ad1180b7b49c65f58cb503210f92ccabc71c2b5
Petr Bednarski <petrbednarski@gmail.com>
2023-05-11 19:52:09 +0200

    Script edited

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,29 @@
 'use strict';
+'use strict';
+
+const btn = document.getElementById('btn');
+const inputek = document.getElementById('inputText');
+const vykricnik = document.querySelector('.exclamation-mark');
+const errorMessage = document.querySelector('.error-message');
+
+btn.addEventListener('click', () => {
+  if (!ValidateEmail(inputText)) {
+    inputek.classList.add('show');
+    vykricnik.classList.add('show');
+    errorMessage.classList.add('show');
+  } else {
+    inputek.classList.remove('show');
+    vykricnik.classList.remove('show');
+    errorMessage.classList.remove('show');
+  }
+});
+
+function ValidateEmail(input) {
+  const validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (input.value.match(validRegex)) {
+    return true;
+  } else {
+    return false;
+  }
+}
